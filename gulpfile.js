16df71ac7a00e20e0060bcef48d989e4788956f7
Mauro <mauro_leao@hotmail.com>
2025-08-27 11:30:09 -0300

    fix bug vercel

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
         
 }
 
@@ -14,14 +16,30 @@ function images() {
         .pipe(gulp.dest('./dist/img'));
 }
 
+function scripts() {
+    return gulp.src('./src/scripts/**/*')
+        .pipe(gulp.dest('./dist/scripts'));
+}
+
 function html() {
     return gulp.src('./index.html')
         .pipe(gulp.dest('./dist'));
 }
 
-exports.default = gulp.series(styles, images, html);
+function serve() {
+    browserSync.init({ server: './dist' });
+
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/img/**/*', images).on('change', browserSync.reload);
+    gulp.watch('./src/scripts/**/*', scripts).on('change', browserSync.reload);
+    gulp.watch('./index.html', html).on('change', browserSync.reload);
+}
+
+exports.default = gulp.series(styles, images, scripts, html);
+exports.dev = gulp.series(styles, images, scripts, html, serve);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/img/**/*', gulp.parallel(images));
+    gulp.watch('./src/scripts/**/*', gulp.parallel(scripts));
     gulp.watch('./index.html', gulp.parallel(html));
 };
